0c213b1e122efc617482a44c9d4d8da3fed85fd3
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-05-06 12:00:52 -0500

    Template Thatch Blocks & Models

---
+++ src/main/java/com/teamabnormals/abnormals_core/common/blocks/ThatchSlabBlock.java
@@ -0,0 +1,30 @@
+package com.teamabnormals.abnormals_core.common.blocks;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class ThatchSlabBlock extends SlabBlock{
+	public ThatchSlabBlock(Properties properties) {
+		super(properties);
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public float getAmbientOcclusionLightValue(BlockState state, IBlockReader worldIn, BlockPos pos) {
+	    return 1.0F;
+	}
+
+	@Override
+	public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+	    return true;
+	}
+
+	@Override
+	public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+	    return false;
+	}
+}
