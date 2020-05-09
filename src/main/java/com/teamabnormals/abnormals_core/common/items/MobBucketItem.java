237e4b97631a789d92d159fd31f08fecce1b9ce3
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-05-09 15:04:38 -0500

    Moved some stuff from UA

---
+++ src/main/java/com/teamabnormals/abnormals_core/common/items/MobBucketItem.java
@@ -0,0 +1,47 @@
+package com.teamabnormals.abnormals_core.common.items;
+
+import java.util.function.Supplier;
+
+import javax.annotation.Nullable;
+
+import com.teamabnormals.abnormals_core.common.entity.BucketableWaterMobEntity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+
+public class MobBucketItem extends BucketItem {
+	private final Supplier<EntityType<? extends BucketableWaterMobEntity>> entityType;
+
+	public MobBucketItem(Supplier<EntityType<? extends BucketableWaterMobEntity>> entityType, Supplier<? extends Fluid> supplier, Item.Properties builder) {
+		super(supplier, builder);
+		this.entityType = entityType;
+	}
+
+	public void onLiquidPlaced(World worldIn, ItemStack stack, BlockPos pos) {
+		if (!worldIn.isRemote) {
+			this.placeEntity(worldIn, stack, pos);
+		}
+	}
+	
+	protected void playEmptySound(@Nullable PlayerEntity player, IWorld worldIn, BlockPos pos) {
+		worldIn.playSound(player, pos, SoundEvents.ITEM_BUCKET_EMPTY_FISH, SoundCategory.NEUTRAL, 1.0F, 1.0F);
+	}
+
+	private void placeEntity(World worldIn, ItemStack stack, BlockPos pos) {
+		Entity entity = this.entityType.get().spawn(worldIn, stack, (PlayerEntity)null, pos, SpawnReason.BUCKET, true, false);
+		if (entity != null) {
+			((BucketableWaterMobEntity)entity).setFromBucket(true);
+		}
+	}
+}
---
+++ src/main/java/com/teamabnormals/abnormals_core/core/library/api/IBucketableEntity.java
@@ -0,0 +1,7 @@
+package com.teamabnormals.abnormals_core.core.library.api;
+
+import net.minecraft.item.ItemStack;
+
+public interface IBucketableEntity {
+    ItemStack getBucket();
+}
