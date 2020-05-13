ee5f9aab0ef6b6a58bc497c6a9c132c4720116e8
SmellyModder <smellysocks543@gmail.com>
2020-05-13 19:46:08 -0400

    Mob Buckets now get inserted after Vanilla's Buckets

--- src/main/java/com/teamabnormals/abnormals_core/common/items/MobBucketItem.java
+++ src/main/java/com/teamabnormals/abnormals_core/common/items/MobBucketItem.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 import javax.annotation.Nullable;
 
 import com.teamabnormals.abnormals_core.common.entity.BucketableWaterMobEntity;
+import com.teamabnormals.abnormals_core.core.utils.ItemStackUtils;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -13,7 +14,10 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
@@ -42,6 +46,18 @@ public class MobBucketItem extends BucketItem {
 		Entity entity = this.entityType.get().spawn(worldIn, stack, (PlayerEntity)null, pos, SpawnReason.BUCKET, true, false);
 		if (entity != null) {
 			((BucketableWaterMobEntity)entity).setFromBucket(true);
+		}
+	}
+	
+	@Override
+	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
+		if(this.isInGroup(group)) {
+			int targetIndex = ItemStackUtils.findIndexOfItem(Items.TROPICAL_FISH_BUCKET, items);
+			if(targetIndex != -1) {
+				items.add(targetIndex + 1, new ItemStack(this));
+			} else {
+				super.fillItemGroup(group, items);
+			}
 		}
 	}
 }
