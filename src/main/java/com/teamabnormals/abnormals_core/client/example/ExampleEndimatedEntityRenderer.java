ad2ec62af76fdb4b756989d0cc9a0e7035293cfe
SmellyModder <smellysocks543@gmail.com>
2020-09-04 22:20:05 -0400

    Added EntitySkinHelper

--- src/main/java/com/teamabnormals/abnormals_core/client/example/ExampleEndimatedEntityRenderer.java
+++ src/main/java/com/teamabnormals/abnormals_core/client/example/ExampleEndimatedEntityRenderer.java
@@ -1,5 +1,6 @@
 package com.teamabnormals.abnormals_core.client.example;
 
+import com.teamabnormals.abnormals_core.client.EntitySkinHelper;
 import com.teamabnormals.abnormals_core.common.entity.ExampleEndimatedEntity;
 import com.teamabnormals.abnormals_core.core.AbnormalsCore;
 
@@ -8,14 +9,16 @@ import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.util.ResourceLocation;
 
 public final class ExampleEndimatedEntityRenderer extends LivingRenderer<ExampleEndimatedEntity, ExampleEndimatedEntityModel<ExampleEndimatedEntity>> {
-	
+	private static final EntitySkinHelper<ExampleEndimatedEntity> SKIN_HELPER = EntitySkinHelper.create(AbnormalsCore.MODID, "textures/entity/skins", "them", (helper) -> {
+		helper.putSkins("dudes", "them", "dudes", "smelly", "test");
+	});
+
 	public ExampleEndimatedEntityRenderer(EntityRendererManager renderManager) {
         super(renderManager, new ExampleEndimatedEntityModel<>(), 0.0F);
     }
 	
 	@Override
 	public ResourceLocation getEntityTexture(ExampleEndimatedEntity entity) {
-		return AbnormalsCore.REGISTRY_HELPER.prefix("textures/entity/example.png");
+		return SKIN_HELPER.getSkinForEntityOrElse(entity, AbnormalsCore.REGISTRY_HELPER.prefix("textures/entity/example.png"));
 	}
-	
 }
