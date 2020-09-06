b587a711b81ef8a5d7e055573e8bf7fc85275132
Jackson <jackson.hardaway@gmail.com>
2020-09-06 14:46:45 -0700

    Formatting

--- src/main/java/com/teamabnormals/abnormals_core/common/network/MessageS2CServerRedirect.java
+++ src/main/java/com/teamabnormals/abnormals_core/common/network/MessageS2CServerRedirect.java
@@ -19,38 +19,38 @@ public final class MessageS2CServerRedirect {
     private static final Minecraft MINECRAFT = Minecraft.getInstance();
     private final String connectionAddress;
 
-	public MessageS2CServerRedirect(String address) {
-		this.connectionAddress = address;
-	}
+    public MessageS2CServerRedirect(String address) {
+        this.connectionAddress = address;
+    }
 
-	public void serialize(PacketBuffer buf) {
-		buf.writeString(this.connectionAddress);
-	}
+    public void serialize(PacketBuffer buf) {
+        buf.writeString(this.connectionAddress);
+    }
 
-	public static MessageS2CServerRedirect deserialize(PacketBuffer buf) {
-		return new MessageS2CServerRedirect(buf.readString());
-	}
+    public static MessageS2CServerRedirect deserialize(PacketBuffer buf) {
+        return new MessageS2CServerRedirect(buf.readString());
+    }
 
-	public static void handle(MessageS2CServerRedirect message, Supplier<NetworkEvent.Context> ctx) {
-		NetworkEvent.Context context = ctx.get();
-		if (context.getDirection().getReceptionSide() == LogicalSide.CLIENT) {
-			context.enqueueWork(() -> {
-				World world = MINECRAFT.world;
-				Screen currentScreen = MINECRAFT.currentScreen;
-				boolean integrated = MINECRAFT.isIntegratedServerRunning();
+    public static void handle(MessageS2CServerRedirect message, Supplier<NetworkEvent.Context> ctx) {
+        NetworkEvent.Context context = ctx.get();
+        if (context.getDirection().getReceptionSide() == LogicalSide.CLIENT) {
+            context.enqueueWork(() -> {
+                World world = MINECRAFT.world;
+                Screen currentScreen = MINECRAFT.currentScreen;
+                boolean integrated = MINECRAFT.isIntegratedServerRunning();
 
-				if (world != null) {
+                if (world != null) {
                     world.sendQuittingDisconnectingPacket();
                     MINECRAFT.unloadWorld(new DirtMessageScreen(new TranslationTextComponent("abnormals_core.message.redirect")));
 
                     MainMenuScreen menuScreen = new MainMenuScreen();
                     MINECRAFT.displayGuiScreen(integrated ? menuScreen : new MultiplayerScreen(menuScreen));
 
-					if (currentScreen != null)
+                    if (currentScreen != null)
                         MINECRAFT.displayGuiScreen(new ConnectingScreen(currentScreen, MINECRAFT, new ServerData("Redirect", message.connectionAddress, false)));
-				}
-			});
-			context.setPacketHandled(true);
-		}
-	}
+                }
+            });
+            context.setPacketHandled(true);
+        }
+    }
 }
