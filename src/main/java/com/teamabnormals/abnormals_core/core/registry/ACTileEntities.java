c9e99e12b3aec70ba5fa52d9797f4ba278004be9
SmellyModder <smellysocks543@gmail.com>
2020-09-09 14:40:44 -0400

    Made TE block collecting dynamic

--- src/main/java/com/teamabnormals/abnormals_core/core/registry/ACTileEntities.java
+++ src/main/java/com/teamabnormals/abnormals_core/core/registry/ACTileEntities.java
@@ -18,30 +18,14 @@ import net.minecraftforge.registries.ForgeRegistries;
 @Mod.EventBusSubscriber(modid = AbnormalsCore.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class ACTileEntities {
 	public static final RegistryHelper HELPER = AbnormalsCore.REGISTRY_HELPER;
-	
-	public static final RegistryObject<TileEntityType<AbnormalsSignTileEntity>> SIGN = HELPER.createTileEntity("sign", AbnormalsSignTileEntity::new, () -> collectSignBlocks());
-	public static final RegistryObject<TileEntityType<AbnormalsBeehiveTileEntity>> BEEHIVE = HELPER.createTileEntity("beehive", AbnormalsBeehiveTileEntity::new, () -> collectBeehiveBlocks());
-	public static final RegistryObject<TileEntityType<AbnormalsChestTileEntity>> CHEST = HELPER.createTileEntity("chest", AbnormalsChestTileEntity::new, () -> collectChestBlocks());
-	public static final RegistryObject<TileEntityType<AbnormalsTrappedChestTileEntity>> TRAPPED_CHEST = HELPER.createTileEntity("trapped_chest", AbnormalsTrappedChestTileEntity::new, () -> collectTrappedChestBlocks());
-	public static final RegistryObject<TileEntityType<ScentedCandleTileEntity>> SCENTED_CANDLE = HELPER.createTileEntity("scented_candle", ScentedCandleTileEntity::new, () -> collectScentedCandleBlocks());
 
-	private static Block[] collectSignBlocks() {
-		return ForgeRegistries.BLOCKS.getValues().stream().filter(block -> block instanceof AbnormalsAbstractSignBlock).toArray(Block[]::new);
-	}
+	public static final RegistryObject<TileEntityType<AbnormalsSignTileEntity>> SIGN = HELPER.createTileEntity("sign", AbnormalsSignTileEntity::new, () -> collectBlocks(AbnormalsAbstractSignBlock.class));
+	public static final RegistryObject<TileEntityType<AbnormalsBeehiveTileEntity>> BEEHIVE = HELPER.createTileEntity("beehive", AbnormalsBeehiveTileEntity::new, () -> collectBlocks(AbnormalsBeehiveBlock.class));
+	public static final RegistryObject<TileEntityType<AbnormalsChestTileEntity>> CHEST = HELPER.createTileEntity("chest", AbnormalsChestTileEntity::new, () -> collectBlocks(AbnormalsChestBlock.class));
+	public static final RegistryObject<TileEntityType<AbnormalsTrappedChestTileEntity>> TRAPPED_CHEST = HELPER.createTileEntity("trapped_chest", AbnormalsTrappedChestTileEntity::new, () -> collectBlocks(AbnormalsTrappedChestBlock.class));
+	public static final RegistryObject<TileEntityType<ScentedCandleTileEntity>> SCENTED_CANDLE = HELPER.createTileEntity("scented_candle", ScentedCandleTileEntity::new, () -> collectBlocks(ScentedCandleBlock.class));
 
-	private static Block[] collectBeehiveBlocks() {
-		return ForgeRegistries.BLOCKS.getValues().stream().filter(block -> block instanceof AbnormalsBeehiveBlock).toArray(Block[]::new);
-	}
-
-	private static Block[] collectChestBlocks() {
-		return ForgeRegistries.BLOCKS.getValues().stream().filter(block -> block instanceof AbnormalsChestBlock).toArray(Block[]::new);
-	}
-
-	private static Block[] collectTrappedChestBlocks() {
-		return ForgeRegistries.BLOCKS.getValues().stream().filter(block -> block instanceof AbnormalsTrappedChestBlock).toArray(Block[]::new);
-	}
-
-	private static Block[] collectScentedCandleBlocks() {
-		return ForgeRegistries.BLOCKS.getValues().stream().filter(block -> block instanceof ScentedCandleBlock).toArray(Block[]::new);
+	private static Block[] collectBlocks(Class<? extends Block> blockClass) {
+		return ForgeRegistries.BLOCKS.getValues().stream().filter(block -> blockClass.isInstance(block)).toArray(Block[]::new);
 	}
 }
