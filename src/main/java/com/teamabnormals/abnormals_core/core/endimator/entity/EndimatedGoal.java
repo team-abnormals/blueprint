ead4c9d3cd84e5f0ac6aebd497b327b24c1c8113
SmellyModder <smellysocks543@gmail.com>
2020-09-09 20:01:09 -0400

    Some improvements to EndimatedGoal

--- src/main/java/com/teamabnormals/abnormals_core/core/endimator/entity/EndimatedGoal.java
+++ src/main/java/com/teamabnormals/abnormals_core/core/endimator/entity/EndimatedGoal.java
@@ -6,33 +6,61 @@ import com.teamabnormals.abnormals_core.core.util.NetworkUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.goal.Goal;
 
+import java.util.Random;
+
 /**
- * A Goal that makes Goals with Endimations simpler
+ * A {@link Goal} that makes goals with Endimations easier.
  * @author SmellyModder(Luke Tonon)
- * @param <E> - The entity for the Goal
+ * @param <E> - The entity for the Goal.
  */
 public abstract class EndimatedGoal<E extends Entity & IEndimatedEntity> extends Goal {
 	protected final E entity;
+	protected final Endimation endimation;
+	protected final Random random;
 	
-	public EndimatedGoal(E entity) {
+	public EndimatedGoal(E entity, Endimation endimation) {
 		this.entity = entity;
+		this.endimation = endimation;
+		this.random = new Random();
+	}
+
+	protected void playEndimation() {
+		NetworkUtil.setPlayingAnimationMessage(this.entity, this.endimation);
+	}
+
+	protected void playEndimation(Endimation endimation) {
+		NetworkUtil.setPlayingAnimationMessage(this.entity, endimation);
 	}
 	
 	protected boolean isEndimationPlaying() {
-		return this.entity.isEndimationPlaying(this.getEndimation());
+		return this.entity.isEndimationPlaying(this.endimation);
+	}
+
+	protected boolean isEndimationPlaying(Endimation endimation) {
+		return this.entity.isEndimationPlaying(endimation);
+	}
+
+	protected boolean isNoEndimationPlaying() {
+		return this.entity.isNoEndimationPlaying();
 	}
 	
 	protected boolean isEndimationAtTick(int tick) {
 		return this.entity.getAnimationTick() == tick;
 	}
+
+	protected boolean isEndimationPastTick(int tick) {
+		return this.entity.getAnimationTick() > tick;
+	}
 	
 	protected boolean isEndimationPastOrAtTick(int tick) {
 		return this.entity.getAnimationTick() >= tick;
 	}
-	
-	protected void playEndimation() {
-		NetworkUtil.setPlayingAnimationMessage(this.entity, this.getEndimation());
+
+	protected boolean isEndimationBeforeTick(int tick) {
+		return this.entity.getAnimationTick() < tick;
 	}
 
-	protected abstract Endimation getEndimation();
+	protected boolean isEndimationBeforeOrAtTick(int tick) {
+		return this.entity.getAnimationTick() <= tick;
+	}
 }
