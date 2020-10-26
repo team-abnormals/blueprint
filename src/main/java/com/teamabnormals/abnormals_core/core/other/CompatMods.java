93be8a51d23d94c874c71900fb3cc230255d795d
EltrutLikes <10eltrut1@gmail.com>
2020-10-26 12:04:11 -0400

    add class with mod id strings

---
+++ src/main/java/com/teamabnormals/abnormals_core/core/other/CompatMods.java
@@ -0,0 +1,32 @@
+package com.teamabnormals.abnormals_core.core.other;
+
+public class CompatMods {
+	
+	public static class VazkiiMods {
+		public static final String QUARK 				= "quark";
+	}
+	
+	public static class AbnormalsMods {
+		public static final String ATMOSPHERIC 			= "atmospheric";
+		public static final String AUTUMNITY 			= "autumnity";
+		public static final String BAMBOO_BLOCKS 		= "bamboo_blocks";
+		public static final String BERRY_GOOD			= "berry_good";
+		public static final String BUZZIER_BEES			= "buzzier_bees";
+		public static final String ENDERGETIC_EXPANSION = "endergetic";
+		public static final String ENVIRONMENTAL 		= "environmental";
+		public static final String EXTRA_BOATS			= "extraboats";
+		public static final String NEAPOLITAN 			= "neapolitan";
+		// public static final String NETHER_EXTENSION = "nether_extension";
+		public static final String SAVAGE_AND_RAVAGE 	= "savageandravage";
+		public static final String UPGRADE_AQUATIC 		= "upgrade_aquatic";
+	}
+	
+	public static class AuroraMods {
+		public static final String BETTER_BADLANDS		= "better_badlands";
+		public static final String ENHANCED_MUSHROOMS	= "enhanced_mushrooms";
+		public static final String FROSTBURN_EXPANSION	= "frostburn_expansion";
+		public static final String FRUITFUL				= "fruitful";
+		public static final String HANAMI 				= "hanami";
+	}
+	
+}
