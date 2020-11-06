89248688246323aaacd7a85931cfa237fb90f269
Jackson <jackson.hardaway@gmail.com>
2020-11-05 19:38:06 -0800

    Made nametag render higher when slabfish is enabled.
    * Had to use mixins due to the event being used on the shadow render

---
+++ src/main/java/com/teamabnormals/abnormals_core/core/mixin/client/PlayerRendererMixin.java
@@ -0,0 +1,37 @@
+package com.teamabnormals.abnormals_core.core.mixin.client;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.teamabnormals.abnormals_core.client.RewardHandler;
+import com.teamabnormals.abnormals_core.common.world.storage.tracking.IDataManager;
+import com.teamabnormals.abnormals_core.core.AbnormalsCore;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.entity.PlayerRenderer;
+import net.minecraft.util.text.ITextComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
+
+@Mixin(PlayerRenderer.class)
+public class PlayerRendererMixin {
+
+	@Inject(method = "renderName", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/matrix/MatrixStack;push()V", shift = At.Shift.AFTER))
+	public void moveName(AbstractClientPlayerEntity entity, ITextComponent name, MatrixStack stack, IRenderTypeBuffer buffer, int packedLight, CallbackInfo ci) {
+		IDataManager data = (IDataManager) entity;
+		UUID uuid = entity.getUniqueID();
+
+		if(!data.getValue(AbnormalsCore.SLABFISH_HEAD) || !RewardHandler.REWARDS.containsKey(uuid))
+			return;
+
+		RewardHandler.RewardData reward = RewardHandler.REWARDS.get(uuid);
+		RewardHandler.RewardData.SlabfishData slabfish = reward.getSlabfish();
+
+		if(slabfish == null || reward.getTier() < 2 || (slabfish.getTypeUrl() == null && RewardHandler.getRewardProperties().getSlabfishProperties().getDefaultTypeUrl() == null))
+			return;
+
+		stack.translate(0, 0.5, 0);
+	}
+}
