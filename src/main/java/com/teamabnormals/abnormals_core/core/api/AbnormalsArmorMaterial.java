f7b3562fd73e52a35f8a72c38f7a7bbd26c407a5
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-11-15 01:17:52 -0600

    Added AbnormalsArmorMaterial

---
+++ src/main/java/com/teamabnormals/abnormals_core/core/api/AbnormalsArmorMaterial.java
@@ -0,0 +1,73 @@
+package com.teamabnormals.abnormals_core.core.api;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+
+import java.util.function.Supplier;
+
+public class AbnormalsArmorMaterial implements IArmorMaterial {
+	private static final int[] MAX_DAMAGE_ARRAY = new int[]{13, 15, 16, 11};
+	private final ResourceLocation name;
+	private final int maxDamageFactor;
+	private final int[] damageReductionAmountArray;
+	private final int enchantability;
+	private final SoundEvent soundEvent;
+	private final float toughness;
+	private final float knockbackResistance;
+	private final LazyValue<Ingredient> repairMaterial;
+
+	public AbnormalsArmorMaterial(ResourceLocation name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability, SoundEvent soundEvent, float toughness, float knockbackResistance, Supplier<Ingredient> repairMaterial) {
+		this.name = name;
+		this.maxDamageFactor = maxDamageFactor;
+		this.damageReductionAmountArray = damageReductionAmountArray;
+		this.enchantability = enchantability;
+		this.soundEvent = soundEvent;
+		this.toughness = toughness;
+		this.knockbackResistance = knockbackResistance;
+		this.repairMaterial = new LazyValue<>(repairMaterial);
+	}
+
+	@Override
+	public int getDurability(EquipmentSlotType slotIn) {
+		return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+	}
+
+	@Override
+	public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+		return this.damageReductionAmountArray[slotIn.getIndex()];
+	}
+
+	@Override
+	public int getEnchantability() {
+		return this.enchantability;
+	}
+
+	@Override
+	public SoundEvent getSoundEvent() {
+		return this.soundEvent;
+	}
+
+	@Override
+	public Ingredient getRepairMaterial() {
+		return this.repairMaterial.getValue();
+	}
+
+	@Override
+	public String getName() {
+		return name.toString();
+	}
+
+	@Override
+	public float getToughness() {
+		return this.toughness;
+	}
+
+	@Override
+	public float getKnockbackResistance() {
+		return this.knockbackResistance;
+	}
+}
