e045a1a60645888515ef6def97091c9ad2f91f49
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-11-15 01:18:03 -0600

    Added AbnormalsItemTier

---
+++ src/main/java/com/teamabnormals/abnormals_core/core/api/AbnormalsItemTier.java
@@ -0,0 +1,55 @@
+package com.teamabnormals.abnormals_core.core.api;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public class AbnormalsItemTier implements IItemTier {
+	private final int harvestLevel;
+	private final int maxUses;
+	private final float efficiency;
+	private final float attackDamage;
+	private final int enchantability;
+	private final LazyValue<Ingredient> repairMaterial;
+
+	public AbnormalsItemTier(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
+		this.harvestLevel = harvestLevelIn;
+		this.maxUses = maxUsesIn;
+		this.efficiency = efficiencyIn;
+		this.attackDamage = attackDamageIn;
+		this.enchantability = enchantabilityIn;
+		this.repairMaterial = new LazyValue<>(repairMaterialIn);
+	}
+
+	@Override
+	public int getMaxUses() {
+		return maxUses;
+	}
+
+	@Override
+	public float getEfficiency() {
+		return efficiency;
+	}
+
+	@Override
+	public float getAttackDamage() {
+		return attackDamage;
+	}
+
+	@Override
+	public int getHarvestLevel() {
+		return harvestLevel;
+	}
+
+	@Override
+	public int getEnchantability() {
+		return enchantability;
+	}
+
+	@Override
+	public Ingredient getRepairMaterial() {
+		return repairMaterial.getValue();
+	}
+}
