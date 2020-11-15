0d588b93fb4d65d259660b9c63a9ffda83f6b17b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-11-15 01:17:30 -0600

    Added AbnormalsEffect

---
+++ src/main/java/com/teamabnormals/abnormals_core/common/potion/AbnormalsEffect.java
@@ -0,0 +1,10 @@
+package com.teamabnormals.abnormals_core.common.potion;
+
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectType;
+
+public class AbnormalsEffect extends Effect {
+	public AbnormalsEffect(EffectType effectType, int liquidColor) {
+		super(effectType, liquidColor);
+	}
+}
