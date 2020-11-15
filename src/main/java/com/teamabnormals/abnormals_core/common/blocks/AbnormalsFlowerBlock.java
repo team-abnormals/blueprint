f181b12dcc1d9f9539ec09b2ffa476bfa79181af
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-11-15 01:20:55 -0600

    Updated AbnormalsFlowerBlock to use Suppliers

--- src/main/java/com/teamabnormals/abnormals_core/common/blocks/AbnormalsFlowerBlock.java
+++ src/main/java/com/teamabnormals/abnormals_core/common/blocks/AbnormalsFlowerBlock.java
@@ -1,18 +1,34 @@
 package com.teamabnormals.abnormals_core.common.blocks;
 
+import com.google.common.base.Supplier;
 import com.teamabnormals.abnormals_core.core.util.item.filling.TargetedItemGroupFiller;
 import net.minecraft.block.FlowerBlock;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Effect;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.NonNullList;
 
 public class AbnormalsFlowerBlock extends FlowerBlock {
 	private static final TargetedItemGroupFiller FILLER = new TargetedItemGroupFiller(() -> Items.WITHER_ROSE);
+	private final Supplier<Effect> stewEffect;
+	private final int stewEffectDuration;
 
-	public AbnormalsFlowerBlock(Effect stewEffect, int stewEffectDuration, Properties properties) {
-		super(stewEffect, stewEffectDuration, properties);
+	public AbnormalsFlowerBlock(Supplier<Effect> stewEffect, int stewEffectDuration, Properties properties) {
+		super(Effects.WEAKNESS, stewEffectDuration, properties);
+		this.stewEffect = stewEffect;
+		this.stewEffectDuration = stewEffectDuration;
+	}
+
+	@Override
+	public Effect getStewEffect() {
+		return this.stewEffect.get();
+	}
+
+	@Override
+	public int getStewEffectDuration() {
+		return this.getStewEffect().isInstant() ? this.stewEffectDuration : this.stewEffectDuration * 20;
 	}
 
 	@Override
