d1020d3030ac4df1e605d309bb1c1f8f759a4f3c
abigailfails <thatvaliantvole@gmail.com>
2020-11-26 17:04:13 +0000

    Clarified IAgeableEntity class requirements

--- src/main/java/com/teamabnormals/abnormals_core/core/api/IAgeableEntity.java
+++ src/main/java/com/teamabnormals/abnormals_core/core/api/IAgeableEntity.java
@@ -1,29 +1,30 @@
 package com.teamabnormals.abnormals_core.core.api;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 
 /***
  * @author abigailfails
- * Use to make an entity that doesn't extend AgeableEntity compatible with Quark's potato poisoning and
+ * Use to make a living entity that doesn't extend AgeableEntity compatible with Quark's potato poisoning and
  * Savage & Ravage's Growth & Youth Potions.
+ * Implemented class must extend LivingEntity
  */
-public interface IAgeableEntity {
+public interface IAgeableEntity{
 
     /**
      * If the entity can grow to a higher stage, resets any progress made towards it (e.g. age timer).
      * */
     void resetGrowthProgress();
 
-/**
- * @param isGrowingUp true checks for a higher growth stage, false checks for a lower one.
- * @return whether the entity has another stage it can grow or regress to depending on isGrowingUp.
- */
- boolean canAge(boolean isGrowingUp);
+    /**
+    * @param isGrowingUp true checks for a higher growth stage, false checks for a lower one.
+    * @return whether the entity has another stage it can grow or regress to depending on isGrowingUp.
+    */
+    boolean canAge(boolean isGrowingUp);
 
     /**
      * Attempts to change the entity's growth stage depending on isGrowingUp.
      * @param isGrowingUp true attempts to grow to a higher stage, false attempts to grow to a lower one.
      * @return the entity it ages into - if growing is implemented such that this doesn't change, it returns itself
      * */
-    Entity attemptAging(boolean isGrowingUp);
+    LivingEntity attemptAging(boolean isGrowingUp);
 }
