9590e666df75daa41312cc0d8ee9f0ebe5aa1b20
abigailfails <thatvaliantvole@gmail.com>
2020-11-27 16:07:50 +0000

    Fixed formatting in IAgeableEntity

--- src/main/java/com/teamabnormals/abnormals_core/core/api/IAgeableEntity.java
+++ src/main/java/com/teamabnormals/abnormals_core/core/api/IAgeableEntity.java
@@ -8,23 +8,23 @@ import net.minecraft.entity.LivingEntity;
  * Savage & Ravage's Growth & Youth Potions.
  * Implemented class must extend LivingEntity
  */
-public interface IAgeableEntity{
+public interface IAgeableEntity {
 
     /**
      * If the entity can grow to a higher stage, resets any progress made towards it (e.g. age timer).
-     * */
+     */
     void resetGrowthProgress();
 
     /**
-    * @param isGrowingUp true checks for a higher growth stage, false checks for a lower one.
-    * @return whether the entity has another stage it can grow or regress to depending on isGrowingUp.
-    */
+     * @param isGrowingUp true checks for a higher growth stage, false checks for a lower one.
+     * @return whether the entity has another stage it can grow or regress to depending on isGrowingUp.
+     */
     boolean canAge(boolean isGrowingUp);
 
     /**
      * Attempts to change the entity's growth stage depending on isGrowingUp.
      * @param isGrowingUp true attempts to grow to a higher stage, false attempts to grow to a lower one.
      * @return the entity it ages into - if growing is implemented such that this doesn't change, it returns itself
-     * */
+     */
     LivingEntity attemptAging(boolean isGrowingUp);
 }
