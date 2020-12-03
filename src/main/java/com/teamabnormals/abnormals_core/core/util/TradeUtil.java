1871a453809adc79dcacdc9247260cdb5dc8cdfa
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-12-03 00:23:02 -0600

    Rework TradeUtil because of redundancy

--- src/main/java/com/teamabnormals/abnormals_core/core/util/TradeUtil.java
+++ src/main/java/com/teamabnormals/abnormals_core/core/util/TradeUtil.java
@@ -1,127 +1,58 @@
 package com.teamabnormals.abnormals_core.core.util;
 
-import java.util.Random;
-
-import net.minecraft.block.Block;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.merchant.villager.VillagerTrades.ITrade;
+import net.minecraft.entity.merchant.villager.VillagerProfession;
+import net.minecraft.entity.merchant.villager.VillagerTrades;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.item.MerchantOffer;
+import net.minecraftforge.common.BasicTrade;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.event.village.WandererTradesEvent;
 
 public final class TradeUtil {
-	public static class ItemsForEmeraldsTrade implements ITrade {
-		private final ItemStack itemstack;
-		private final int stackSize;
-		private final int recievedSize;
-		private final int maxUses;
-		private final int givenExp;
-		private final float priceMultiplier;
-
-		public ItemsForEmeraldsTrade(Block block, int stackSize, int recievedSize, int maxUses, int givenExp) {
-			this(new ItemStack(block), stackSize, recievedSize, maxUses, givenExp);
+	public static class AbnormalsTrade extends BasicTrade {
+		public AbnormalsTrade(ItemStack input, ItemStack input2, ItemStack output, int maxTrades, int xp, float priceMult) {
+			super(input, input2, output, maxTrades, xp, priceMult);
 		}
 
-		public ItemsForEmeraldsTrade(Item item, int stackSize, int recievedSize, int givenExp) {
-			this(new ItemStack(item), stackSize, recievedSize, 12, givenExp);
+		public AbnormalsTrade(Item input, int inputCount, Item output, int outputCount, int maxTrades, int xp, float priceMult) {
+			this(new ItemStack(input, inputCount), ItemStack.EMPTY, new ItemStack(output, outputCount), maxTrades, xp, priceMult);
 		}
 
-		public ItemsForEmeraldsTrade(Item item, int stackSize, int recievedSize, int maxUses, int givenExp) {
-			this(new ItemStack(item), stackSize, recievedSize, maxUses, givenExp);
+		public AbnormalsTrade(Item input, int inputCount, Item output, int outputCount, int maxTrades, int xp) {
+			this(input, inputCount, output, outputCount, maxTrades, xp, 0.15F);
 		}
 
-		public ItemsForEmeraldsTrade(ItemStack stack, int stackSize, int recievedSize, int maxUses, int givenExp) {
-			this(stack, stackSize, recievedSize, maxUses, givenExp, 0.05F);
+		public AbnormalsTrade(Item input, int inputCount, int emeraldCount, int maxTrades, int xp, float priceMult) {
+			this(new ItemStack(input, inputCount), ItemStack.EMPTY, new ItemStack(Items.EMERALD, emeraldCount), maxTrades, xp, priceMult);
 		}
 
-		public ItemsForEmeraldsTrade(ItemStack stack, int stackSize, int recievedSize, int maxUses, int givenExp, float priceMultiplier) {
-			this.itemstack = stack;
-			this.stackSize = stackSize;
-			this.recievedSize = recievedSize;
-			this.maxUses = maxUses;
-			this.givenExp = givenExp;
-			this.priceMultiplier = priceMultiplier;
+		public AbnormalsTrade(Item input, int inputCount, int emeraldCount, int maxTrades, int xp) {
+			this(input, inputCount, emeraldCount, maxTrades, xp, 0.15F);
 		}
 
-		public MerchantOffer getOffer(Entity p_221182_1_, Random p_221182_2_) {
-			return new MerchantOffer(new ItemStack(Items.EMERALD, this.stackSize), new ItemStack(this.itemstack.getItem(), this.recievedSize), this.maxUses, this.givenExp, this.priceMultiplier);
+		public AbnormalsTrade(int emeraldCount, Item output, int outputCount, int maxTrades, int xp, float priceMult) {
+			this(new ItemStack(Items.EMERALD, emeraldCount), ItemStack.EMPTY, new ItemStack(output, outputCount), maxTrades, xp, priceMult);
+		}
+
+		public AbnormalsTrade(int emeraldCount, Item output, int outputCount, int maxTrades, int xp) {
+			this(emeraldCount, output, outputCount, maxTrades, xp, 0.15F);
 		}
 	}
 
-	public static class EmeraldsForItemsTrade implements ITrade {
-		private final ItemStack itemstack;
-		private final int stackSize;
-		private final int recievedSize;
-		private final int maxUses;
-		private final int givenExp;
-		private final float priceMultiplier;
-
-		public EmeraldsForItemsTrade(Block block, int stackSize, int recievedSize, int maxUses, int givenExp) {
-			this(new ItemStack(block), stackSize, recievedSize, maxUses, givenExp);
-		}
-
-		public EmeraldsForItemsTrade(Item item, int stackSize, int recievedSize, int givenExp) {
-			this(new ItemStack(item), stackSize, recievedSize, 12, givenExp);
-		}
-
-		public EmeraldsForItemsTrade(Item item, int stackSize, int recievedSize, int maxUses, int givenExp) {
-			this(new ItemStack(item), stackSize, recievedSize, maxUses, givenExp);
-		}
-
-		public EmeraldsForItemsTrade(ItemStack stack, int stackSize, int recievedSize, int maxUses, int givenExp) {
-			this(stack, stackSize, recievedSize, maxUses, givenExp, 0.05F);
-		}
-
-		public EmeraldsForItemsTrade(ItemStack stack, int stackSize, int recievedSize, int maxUses, int givenExp, float priceMultiplier) {
-			this.itemstack = stack;
-			this.stackSize = stackSize;
-			this.recievedSize = recievedSize;
-			this.maxUses = maxUses;
-			this.givenExp = givenExp;
-			this.priceMultiplier = priceMultiplier;
-		}
-
-		public MerchantOffer getOffer(Entity p_221182_1_, Random p_221182_2_) {
-			return new MerchantOffer(new ItemStack(this.itemstack.getItem(), this.stackSize), new ItemStack(Items.EMERALD, this.recievedSize), this.maxUses, this.givenExp, this.priceMultiplier);
-		}
+	public static void addVillagerTrade(VillagerTradesEvent event, int level, VillagerTrades.ITrade trade) {
+		event.getTrades().get(level).add(trade);
 	}
 
-	public static class ItemsForEmeraldsAndItemsTrade implements ITrade {
-		private final ItemStack buyingItem;
-		private final int buyingItemCount;
-		private final int emeraldCount;
-		private final ItemStack sellingItem;
-		private final int sellingItemCount;
-		private final int maxUses;
-		private final int givenExp;
-		private final float priceMultiplier;
+	public static void addVillagerTrade(VillagerTradesEvent event, int level, VillagerProfession profession, VillagerTrades.ITrade trade) {
+		if (event.getType() == profession) addVillagerTrade(event, level, trade);
+	}
 
-		public ItemsForEmeraldsAndItemsTrade(Item buyingItem, int buyingItemCount, int emeraldCount, Item sellingItem, int sellingItemCount, int maxUses, int givenExp) {
-			this(new ItemStack(buyingItem), buyingItemCount, emeraldCount, new ItemStack(sellingItem), sellingItemCount, maxUses, givenExp, 0.05F);
-		}
+	public static void addWandererTrade(WandererTradesEvent event, VillagerTrades.ITrade trade) {
+		event.getGenericTrades().add(trade);
+	}
 
-		public ItemsForEmeraldsAndItemsTrade(Block buyingItem, int buyingItemCount, int emeraldCount, Block sellingItem, int sellingItemCount, int maxUses, int givenExp) {
-			this(new ItemStack(buyingItem), buyingItemCount, emeraldCount, new ItemStack(sellingItem), sellingItemCount, maxUses, givenExp, 0.05F);
-		}
-
-		public ItemsForEmeraldsAndItemsTrade(Block buyingItem) {
-			this(new ItemStack(buyingItem), 1, 5, new ItemStack(buyingItem), 2, 6, 30, 0.05F);
-		}
-
-		public ItemsForEmeraldsAndItemsTrade(ItemStack buyingItem, int buyingItemCount, int emeraldCount, ItemStack sellingItem, int sellingItemCount, int maxUses, int givenExp, float priceMultiplier) {
-			this.buyingItem = buyingItem;
-			this.buyingItemCount = buyingItemCount;
-			this.emeraldCount = emeraldCount;
-			this.sellingItem = sellingItem;
-			this.sellingItemCount = sellingItemCount;
-			this.maxUses = maxUses;
-			this.givenExp = givenExp;
-			this.priceMultiplier = priceMultiplier;
-		}
-
-		public MerchantOffer getOffer(Entity p_221182_1_, Random p_221182_2_) {
-			return new MerchantOffer(new ItemStack(Items.EMERALD, this.emeraldCount), new ItemStack(this.buyingItem.getItem(), this.buyingItemCount), new ItemStack(this.sellingItem.getItem(), this.sellingItemCount), this.maxUses, this.givenExp, this.priceMultiplier);
-		}
+	public static void addRareWandererTrade(WandererTradesEvent event, VillagerTrades.ITrade trade) {
+		event.getRareTrades().add(trade);
 	}
 }
