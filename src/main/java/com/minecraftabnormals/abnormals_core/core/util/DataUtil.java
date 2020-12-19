023c7dffd2c363c869efc06c560aab880e915d03
abigailfails <thatvaliantvole@gmail.com>
2020-12-19 10:15:54 +0000

    Added registerConditionalDispenseBehavior method

--- src/main/java/com/minecraftabnormals/abnormals_core/core/util/DataUtil.java
+++ src/main/java/com/minecraftabnormals/abnormals_core/core/util/DataUtil.java
@@ -2,16 +2,23 @@ package com.minecraftabnormals.abnormals_core.core.util;
 
 import java.lang.reflect.Array;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ComposterBlock;
 import net.minecraft.block.FireBlock;
+import net.minecraft.block.DispenserBlock;
 import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IDispenseItemBehavior;
 import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IItemProvider;
 import net.minecraftforge.fml.RegistryObject;
 
@@ -57,6 +64,20 @@ public final class DataUtil {
 		System.arraycopy(array, 0, newArrayObject, 0, arrayLength);
 		array[array.length - 1] = element;
 		return array;
+	}
+
+	/**
+	 * <p>Registers a {@link IDispenseItemBehavior} that will perform the new behavior if the condition is met
+	 * and the behavior that was already in the registry if not.</p>
+	 * This works even if multiple mods add new behavior to the same item.
+	 *
+	 * @author abigailfails
+	 * */
+	public static void registerConditionalDispenseBehavior(Item item, BiPredicate<IBlockSource, ItemStack> condition, DefaultDispenseItemBehavior newBehavior) {
+		IDispenseItemBehavior oldBehavior = DispenserBlock.DISPENSE_BEHAVIOR_REGISTRY.get(item);
+		DispenserBlock.registerDispenseBehavior(item, (source, stack) -> {
+			return condition.test(source, stack) ? newBehavior.dispense(source, stack) : oldBehavior.dispense(source, stack);
+		});
 	}
 
 }
