e9d2466e9598e3a9382bf2fa475f71f5c47c1bda
abigailfails <thatvaliantvole@gmail.com>
2020-12-20 10:05:47 +0000

    Apply suggestions from code review
    Co-authored-by: Luke Tonon <41268323+SmellyModder@users.noreply.github.com>

--- src/main/java/com/minecraftabnormals/abnormals_core/core/util/DataUtil.java
+++ src/main/java/com/minecraftabnormals/abnormals_core/core/util/DataUtil.java
@@ -76,12 +76,12 @@ public final class DataUtil {
 	}
 
 	/**
-	 * <p>Registers a {@link IDispenseItemBehavior} that will perform the new behavior if the condition is met
-	 * and the behavior that was already in the registry if not.</p>
+	 * <p>Registers a {@link IDispenseItemBehavior} that will perform the new behavior if the condition is met and the behavior that was already in the registry if not.</p>
 	 * This works even if multiple mods add new behavior to the same item.
 	 *
 	 * @author abigailfails
-	 * */
+	 *
+	 */
 	public static void registerConditionalDispenseBehavior(Item item, BiPredicate<IBlockSource, ItemStack> condition, IDispenseItemBehavior newBehavior) {
 		IDispenseItemBehavior oldBehavior = DispenserBlock.DISPENSE_BEHAVIOR_REGISTRY.get(item);
 		DispenserBlock.registerDispenseBehavior(item, (source, stack) -> {
