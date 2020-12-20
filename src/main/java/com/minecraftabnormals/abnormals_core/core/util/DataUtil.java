83ae7001dcb6bcec7e241011aae1c62224c38a9f
abigailfails <thatvaliantvole@gmail.com>
2020-12-20 20:39:08 +0000

    Added a method for registering a dispense behavior that does not override the old one (#55)
    * Added registerConditionalDispenseBehavior method
    
    * Resolved merge conflicts
    
    * Apply suggestions from code review
    
    Co-authored-by: Luke Tonon <41268323+SmellyModder@users.noreply.github.com>
    
    * Optimised imports and changed method name
    
    * Fixed merge conflict
    
    * Added parameters to javadocs
    
    * Readded other change
    
    * Apply suggestions from code review
    
    Co-authored-by: Luke Tonon <41268323+SmellyModder@users.noreply.github.com>
    
    * Moved explanation from parameter to main method javadoc
    
    * Update src/main/java/com/minecraftabnormals/abnormals_core/core/util/DataUtil.java
    
    Co-authored-by: Luke Tonon <41268323+SmellyModder@users.noreply.github.com>
    
    Co-authored-by: Luke Tonon <41268323+SmellyModder@users.noreply.github.com>

--- src/main/java/com/minecraftabnormals/abnormals_core/core/util/DataUtil.java
+++ src/main/java/com/minecraftabnormals/abnormals_core/core/util/DataUtil.java
@@ -3,12 +3,17 @@ package com.minecraftabnormals.abnormals_core.core.util;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ComposterBlock;
+import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.FireBlock;
 import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.client.renderer.color.ItemColors;
+import net.minecraft.dispenser.IBlockSource;
+import net.minecraft.dispenser.IDispenseItemBehavior;
 import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
@@ -16,6 +21,7 @@ import net.minecraftforge.fml.RegistryObject;
 
 import java.lang.reflect.Array;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 public final class DataUtil {
 
@@ -71,5 +77,23 @@ public final class DataUtil {
 			if (key.getLocation().equals(loc))
 				return true;
 		return false;
+	}
+
+	/**
+	 * <p>Registers a {@link IDispenseItemBehavior} that will perform the new behavior if the condition is met and the behavior that was already in the registry if not.
+	 * This works even if multiple mods add new behavior to the same item.</p>
+	 * <p>Ideally, the condition should be implemented such that the predicate only passes if the new behavior will be 'successful', avoiding problems with failure sounds not playing.</p>
+	 *
+	 * @param item The {@link Item} to register the {@code newBehavior} for.
+	 * @param condition A {@link BiPredicate} that takes in {@link IBlockSource} and {@link ItemStack} arguments, returning true if the {@code newBehavior} should be performed.
+	 * @param newBehavior The {@link IDispenseItemBehavior} that will be used if the {@code condition} is met.
+	 *
+	 * @author abigailfails
+	 */
+	public static void registerAlternativeDispenseBehavior(Item item, BiPredicate<IBlockSource, ItemStack> condition, IDispenseItemBehavior newBehavior) {
+		IDispenseItemBehavior oldBehavior = DispenserBlock.DISPENSE_BEHAVIOR_REGISTRY.get(item);
+		DispenserBlock.registerDispenseBehavior(item, (source, stack) -> {
+			return condition.test(source, stack) ? newBehavior.dispense(source, stack) : oldBehavior.dispense(source, stack);
+		});
 	}
 }
