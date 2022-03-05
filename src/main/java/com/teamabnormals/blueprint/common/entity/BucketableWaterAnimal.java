19d7bc7c0492380bc517a14ba0684dab169ebc04
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-03-05 12:31:45 -0600

    Remove WaterAnimalBucketItem & IBucketableEntity in favor of vanilla methods

--- src/main/java/com/teamabnormals/blueprint/common/entity/BucketableWaterAnimal.java
+++ src/main/java/com/teamabnormals/blueprint/common/entity/BucketableWaterAnimal.java
@@ -1,101 +1,79 @@
 package com.teamabnormals.blueprint.common.entity;
 
-import com.teamabnormals.blueprint.core.api.IBucketableEntity;
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Bucketable;
 import net.minecraft.world.entity.animal.WaterAnimal;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 
 /**
- * A {@link WaterAnimal} extension that implements {@link IBucketableEntity}.
+ * A {@link WaterAnimal} extension that implements {@link Bucketable}.
  * <p>This is simply a {@link WaterAnimal} that can be bucketed.</p>
- *
- * @see IBucketableEntity
  */
-public abstract class BucketableWaterAnimal extends WaterAnimal implements IBucketableEntity {
+public abstract class BucketableWaterAnimal extends WaterAnimal implements Bucketable {
 	private static final EntityDataAccessor<Boolean> FROM_BUCKET = SynchedEntityData.defineId(BucketableWaterAnimal.class, EntityDataSerializers.BOOLEAN);
 
 	public BucketableWaterAnimal(EntityType<? extends BucketableWaterAnimal> type, Level level) {
 		super(type, level);
 	}
 
+	@Override
 	protected void defineSynchedData() {
 		super.defineSynchedData();
 		this.entityData.define(FROM_BUCKET, false);
 	}
 
-	protected void setBucketData(ItemStack bucket) {
-		if (this.hasCustomName()) {
-			bucket.setHoverName(this.getCustomName());
-		}
-	}
-
-	public void addAdditionalSaveData(CompoundTag compound) {
-		super.addAdditionalSaveData(compound);
-		compound.putBoolean("FromBucket", this.isFromBucket());
-	}
-
-	public void readAdditionalSaveData(CompoundTag compound) {
-		super.readAdditionalSaveData(compound);
-		this.setFromBucket(compound.getBoolean("FromBucket"));
-	}
-
-	public boolean isFromBucket() {
+	@Override
+	public boolean fromBucket() {
 		return this.entityData.get(FROM_BUCKET);
 	}
 
+	@Override
 	public void setFromBucket(boolean value) {
 		this.entityData.set(FROM_BUCKET, value);
 	}
 
-	protected SoundEvent getBucketFillSound() {
-		return SoundEvents.BUCKET_FILL_FISH;
+	@Override
+	public void addAdditionalSaveData(CompoundTag tag) {
+		super.addAdditionalSaveData(tag);
+		tag.putBoolean("FromBucket", this.fromBucket());
+	}
+
+	@Override
+	public void readAdditionalSaveData(CompoundTag tag) {
+		super.readAdditionalSaveData(tag);
+		this.setFromBucket(tag.getBoolean("FromBucket"));
 	}
 
 	@Override
 	public InteractionResult mobInteract(Player player, InteractionHand hand) {
-		ItemStack itemstack = player.getItemInHand(hand);
-		if (itemstack.getItem() == Items.WATER_BUCKET && this.isAlive()) {
-			this.playSound(this.getBucketFillSound(), 1.0F, 1.0F);
-			itemstack.shrink(1);
-			ItemStack itemstack1 = this.getBucket();
-			this.setBucketData(itemstack1);
-			if (!this.level.isClientSide) {
-				CriteriaTriggers.FILLED_BUCKET.trigger((ServerPlayer) player, itemstack1);
-			}
-
-			if (itemstack.isEmpty()) {
-				player.setItemInHand(hand, itemstack1);
-			} else if (!player.getInventory().add(itemstack1)) {
-				player.drop(itemstack1, false);
-			}
-
-			this.kill();
-			return InteractionResult.SUCCESS;
-		} else {
-			return super.mobInteract(player, hand);
-		}
+		return Bucketable.bucketMobPickup(player, hand, this).orElse(super.mobInteract(player, hand));
 	}
 
 	@Override
-	public boolean removeWhenFarAway(double distanceToClosestPlayer) {
-		return !this.isFromBucket() && !this.hasCustomName();
+	public void saveToBucketTag(ItemStack stack) {
+		Bucketable.saveDefaultDataToBucketTag(this, stack);
+	}
+
+	@Override
+	public void loadFromBucketTag(CompoundTag tag) {
+		Bucketable.loadDefaultDataFromBucketTag(this, tag);
 	}
 
 	@Override
 	public boolean requiresCustomPersistence() {
-		return this.isFromBucket();
+		return super.requiresCustomPersistence() || this.fromBucket();
+	}
+
+	@Override
+	public boolean removeWhenFarAway(double distanceToClosestPlayer) {
+		return !this.fromBucket() && !this.hasCustomName();
 	}
 }
