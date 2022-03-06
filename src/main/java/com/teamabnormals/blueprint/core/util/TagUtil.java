a3118a7d2c9a94e6bb6e49b0ba6a9965834d74fd
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-03-06 12:23:58 -0600

    New TagUtil methods

--- src/main/java/com/teamabnormals/blueprint/core/util/TagUtil.java
+++ src/main/java/com/teamabnormals/blueprint/core/util/TagUtil.java
@@ -2,21 +2,21 @@ package com.teamabnormals.blueprint.core.util;
 
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.EntityTypeTags;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.Tag;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
-import net.minecraftforge.common.ForgeTagHandler;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.levelgen.carver.ConfiguredWorldCarver;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 /**
  * A class containing some simple methods for making tags.
@@ -53,7 +53,31 @@ public final class TagUtil {
 		return TagKey.create(Registry.MOB_EFFECT_REGISTRY, new ResourceLocation(modid, name));
 	}
 
-	public static TagKey<NoiseGeneratorSettings> noiseSettingsTag(String modid, String name) {
-		return TagKey.create(Registry.NOISE_GENERATOR_SETTINGS_REGISTRY, new ResourceLocation(modid, name));
+	public static TagKey<Biome> biomeTag(String modid, String name) {
+		return TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(modid, name));
+	}
+
+	public static TagKey<Level> dimensionTag(String modid, String name) {
+		return TagKey.create(Registry.DIMENSION_REGISTRY, new ResourceLocation(modid, name));
+	}
+
+	public static TagKey<DimensionType> dimensionTypeTag(String modid, String name) {
+		return TagKey.create(Registry.DIMENSION_TYPE_REGISTRY, new ResourceLocation(modid, name));
+	}
+
+	public static TagKey<ConfiguredFeature<?, ?>> configuredFeatureTag(String modid, String name) {
+		return TagKey.create(Registry.CONFIGURED_FEATURE_REGISTRY, new ResourceLocation(modid, name));
+	}
+
+	public static TagKey<PlacedFeature> placedFeatureTag(String modid, String name) {
+		return TagKey.create(Registry.PLACED_FEATURE_REGISTRY, new ResourceLocation(modid, name));
+	}
+
+	public static TagKey<ConfiguredStructureFeature<?, ?>> configuredStructureTag(String modid, String name) {
+		return TagKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(modid, name));
+	}
+
+	public static TagKey<ConfiguredWorldCarver<?>> configuredCarverTag(String modid, String name) {
+		return TagKey.create(Registry.CONFIGURED_CARVER_REGISTRY, new ResourceLocation(modid, name));
 	}
 }
