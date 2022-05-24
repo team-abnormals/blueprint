fe1c1c4eb03e1d2de056f5cf1a45d4f9b8413570
SmellyModder <smellysocks543@gmail.com>
2022-05-24 14:40:25 -0400

    Reworked modded end biome test

--- src/test/java/core/data/server/TestModdedBiomeSlicesProvider.java
+++ src/test/java/core/data/server/TestModdedBiomeSlicesProvider.java
@@ -14,8 +14,8 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.CheckerboardColumnBiomeSource;
 import net.minecraft.world.level.biome.Climate;
-import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
 import net.minecraftforge.common.crafting.conditions.ModLoadedCondition;
 
 import java.util.List;
@@ -32,21 +32,14 @@ public final class TestModdedBiomeSlicesProvider extends ModdedBiomeSliceProvide
 		RegistryAccess.Frozen registryAccess = RegistryAccess.BUILTIN.get();
 		var biomes = registryAccess.registryOrThrow(Registry.BIOME_REGISTRY);
 		this.registerSlice(
-				"end_lush_caves",
+				"end_checkerboard",
 				new ConditionedResourceSelector(new NamesResourceSelector("minecraft:the_end"), new ModLoadedCondition(Blueprint.MOD_ID)),
 				5,
 				new BiomeUtil.OverlayModdedBiomeProvider(
 						List.of(
 								Pair.of(
 										biomes.getOrCreateTag(BiomeTags.HAS_END_CITY),
-										new MultiNoiseBiomeSource(
-												new Climate.ParameterList<>(
-														List.of(
-																Pair.of(Climate.parameters(zero, zero, zero, zero, Climate.Parameter.span(0.4F, 0.7F), zero, 0.0F), biomes.getHolderOrThrow(Biomes.LUSH_CAVES)),
-																Pair.of(Climate.parameters(zero, zero, zero, zero, zero, zero, 0.0F), biomes.getHolderOrThrow(BlueprintBiomes.ORIGINAL_SOURCE_MARKER.getKey()))
-														)
-												)
-										)
+										new CheckerboardColumnBiomeSource(biomes.getOrCreateTag(BiomeTags.HAS_STRONGHOLD), 2)
 								)
 						)
 				)
