aa3daa53dbd72d55db17bf3f8225e9b5a0b1a88b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-09-01 15:33:04 -0500

    Improve TreeUtil

--- src/main/java/com/teamabnormals/blueprint/common/block/LeafPileBlock.java
+++ src/main/java/com/teamabnormals/blueprint/common/block/LeafPileBlock.java
@@ -2,14 +2,10 @@ package com.teamabnormals.blueprint.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.MultifaceBlock;
 import net.minecraft.world.level.block.MultifaceSpreader;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
@@ -20,15 +16,12 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 
-import java.util.stream.Stream;
-
 /**
  * A {@link Block} extension for leaf pile compatibility with the Woodworks mod.
  */
 @SuppressWarnings("deprecation")
-public class LeafPileBlock extends MultifaceBlock implements BonemealableBlock, SimpleWaterloggedBlock {
+public class LeafPileBlock extends MultifaceBlock implements SimpleWaterloggedBlock {
 	private static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-	private final MultifaceSpreader spreader = new MultifaceSpreader(this);
 
 	public LeafPileBlock(Properties properties) {
 		super(properties);
@@ -54,7 +47,7 @@ public class LeafPileBlock extends MultifaceBlock implements BonemealableBlock, 
 
 	@Override
 	public MultifaceSpreader getSpreader() {
-		return this.spreader;
+		return null;
 	}
 
 	@Override
@@ -65,20 +58,5 @@ public class LeafPileBlock extends MultifaceBlock implements BonemealableBlock, 
 	@Override
 	public boolean propagatesSkylightDown(BlockState state, BlockGetter level, BlockPos pos) {
 		return state.getFluidState().isEmpty();
-	}
-
-	@Override
-	public boolean isValidBonemealTarget(BlockGetter level, BlockPos pos, BlockState state, boolean isClientSide) {
-		return Stream.of(DIRECTIONS).anyMatch((direction) -> this.spreader.canSpreadInAnyDirection(state, level, pos, direction.getOpposite()));
-	}
-
-	@Override
-	public boolean isBonemealSuccess(Level p_220878_, RandomSource p_220879_, BlockPos p_220880_, BlockState p_220881_) {
-		return true;
-	}
-
-	@Override
-	public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
-		this.spreader.spreadFromRandomFaceTowardRandomDirection(state, level, pos, random);
 	}
 }
--- src/main/java/com/teamabnormals/blueprint/core/util/TreeUtil.java
+++ src/main/java/com/teamabnormals/blueprint/core/util/TreeUtil.java
@@ -1,5 +1,7 @@
 package com.teamabnormals.blueprint.core.util;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
@@ -11,11 +13,15 @@ import net.minecraft.world.level.LevelSimulatedReader;
 import net.minecraft.world.level.LevelWriter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.levelgen.feature.TreeFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * A utility class containing some useful methods for trees.
@@ -70,7 +76,7 @@ public final class TreeUtil {
 	 * @param config A {@link TreeConfiguration} to select the foliage state.
 	 */
 	public static void placeLeafAt(LevelSimulatedRW level, BlockPos pos, RandomSource rand, TreeConfiguration config) {
-		if (isAirOrLeaves(level, pos)) {
+		if (TreeFeature.validTreePos(level, pos)) {
 			setForcedState(level, pos, config.foliageProvider.getState(rand, pos));
 		}
 	}
@@ -143,5 +149,54 @@ public final class TreeUtil {
 	 */
 	public static boolean isValidGround(LevelAccessor level, BlockPos pos, SaplingBlock sapling) {
 		return level.getBlockState(pos).canSustainPlant(level, pos, Direction.UP, sapling);
+	}
+
+	/**
+	 * Updates the distances of nearby leaves around a set of logPositions
+	 *
+	 * @param level        A {@link LevelAccessor} to use.
+	 * @param logPositions A {@link Set} of {@link BlockPos} where logs are at.
+	 */
+	public static void updateLeaves(LevelAccessor level, Set<BlockPos> logPositions) {
+		List<Set<BlockPos>> list = Lists.newArrayList();
+
+		for (int j = 0; j < 6; ++j) {
+			list.add(Sets.newHashSet());
+		}
+		BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
+
+		for (BlockPos pos : Lists.newArrayList(logPositions)) {
+			for (Direction direction : Direction.values()) {
+				mutablePos.setWithOffset(pos, direction);
+				if (!logPositions.contains(mutablePos)) {
+					BlockState state = level.getBlockState(mutablePos);
+					if (state.hasProperty(BlockStateProperties.DISTANCE)) {
+						list.get(0).add(mutablePos.immutable());
+						TreeUtil.setForcedState(level, mutablePos, state.setValue(BlockStateProperties.DISTANCE, 1));
+					}
+				}
+			}
+		}
+
+		for (int l = 1; l < 6; ++l) {
+			Set<BlockPos> set = list.get(l - 1);
+			Set<BlockPos> set1 = list.get(l);
+
+			for (BlockPos pos : set) {
+				for (Direction direction1 : Direction.values()) {
+					mutablePos.setWithOffset(pos, direction1);
+					if (!set.contains(mutablePos) && !set1.contains(mutablePos)) {
+						BlockState state = level.getBlockState(mutablePos);
+						if (state.hasProperty(BlockStateProperties.DISTANCE)) {
+							int k = state.getValue(BlockStateProperties.DISTANCE);
+							if (k > l + 1) {
+								TreeUtil.setForcedState(level, mutablePos, state.setValue(BlockStateProperties.DISTANCE, l + 1));
+								set1.add(mutablePos.immutable());
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
