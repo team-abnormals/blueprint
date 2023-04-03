5c40958574eec98df451b058c4c23f08f83b172b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2023-04-03 14:29:26 -0500

    Formatting tweaks

--- src/main/java/com/teamabnormals/blueprint/core/mixin/FallingBlockEntityMixin.java
+++ src/main/java/com/teamabnormals/blueprint/core/mixin/FallingBlockEntityMixin.java
@@ -16,19 +16,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(FallingBlockEntity.class)
 public abstract class FallingBlockEntityMixin extends Entity {
 
-    public FallingBlockEntityMixin(EntityType<?> type, Level level) {
-        super(type, level);
-    }
+	public FallingBlockEntityMixin(EntityType<?> type, Level level) {
+		super(type, level);
+	}
 
-    @ModifyVariable(method = "fall", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", shift = At.Shift.BEFORE))
-    private static FallingBlockEntity fall(FallingBlockEntity fallingBlockEntity, Level level, BlockPos pos, BlockState state) {
-        return FallingBlockEvent.onBlockFall(level, pos, state, fallingBlockEntity);
-    }
+	@ModifyVariable(method = "fall", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z", shift = At.Shift.BEFORE))
+	private static FallingBlockEntity fall(FallingBlockEntity fallingBlockEntity, Level level, BlockPos pos, BlockState state) {
+		return FallingBlockEvent.onBlockFall(level, pos, state, fallingBlockEntity);
+	}
 
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/FallingBlockEntity;move(Lnet/minecraft/world/entity/MoverType;Lnet/minecraft/world/phys/Vec3;)V", shift = At.Shift.BEFORE), cancellable = true)
-    private void tick(CallbackInfo info) {
-        if (FallingBlockEvent.onFallingBlockTick((FallingBlockEntity) (Object) this)) {
-            info.cancel();
-        }
-    }
+	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/FallingBlockEntity;move(Lnet/minecraft/world/entity/MoverType;Lnet/minecraft/world/phys/Vec3;)V", shift = At.Shift.BEFORE), cancellable = true)
+	private void tick(CallbackInfo info) {
+		if (FallingBlockEvent.onFallingBlockTick((FallingBlockEntity) (Object) this)) {
+			info.cancel();
+		}
+	}
 }
