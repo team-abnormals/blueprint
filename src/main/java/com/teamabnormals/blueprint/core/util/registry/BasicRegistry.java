9d249eb43098a60697b5163759d338a926edcc25
Ebo <eboblox@gmail.com>
2023-12-18 17:25:05 -0500

    add more functionality to BasicRegistry (#211)
    * BasicRegistry improvements
    
    * fix inconsistent javadocs
    
    * Improve id registry efficiency; cleanup methods & docs
    
    * Final simplification

--- src/main/java/com/teamabnormals/blueprint/core/util/registry/BasicRegistry.java
+++ src/main/java/com/teamabnormals/blueprint/core/util/registry/BasicRegistry.java
@@ -7,30 +7,43 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.Lifecycle;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenCustomHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+import net.minecraft.Util;
+import net.minecraft.core.IdMap;
 import net.minecraft.resources.ResourceLocation;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 /**
  * A simplified version of the {@link net.minecraft.core.Registry} class.
- * <p>This class is not an instance of {@link net.minecraft.core.Registry}</p>
+ * <p>This class is not an instance of {@link net.minecraft.core.Registry}.</p>
+ * <p>Values can be added anytime, independent of other registry systems.</p>
  *
  * @param <T> The type of object for the registry.
  * @author SmellyModder (Luke Tonon)
+ * @author ebo2022
  */
-public final class BasicRegistry<T> implements Codec<T> {
+public final class BasicRegistry<T> implements Codec<T>, IdMap<T> {
 	private final Lifecycle lifecycle;
 	private final BiMap<String, T> map = HashBiMap.create();
+	private final ObjectList<T> byId = new ObjectArrayList<>();
+	private final Object2IntMap<T> toId = new Object2IntOpenCustomHashMap<>(Util.identityStrategy());
+	private int nextId;
 
 	public BasicRegistry(Lifecycle lifecycle) {
 		this.lifecycle = lifecycle;
+		this.toId.defaultReturnValue(IdMap.DEFAULT);
 	}
 
 	public BasicRegistry() {
-		this.lifecycle = Lifecycle.stable();
+		this(Lifecycle.stable());
 	}
 
 	/**
@@ -40,7 +53,11 @@ public final class BasicRegistry<T> implements Codec<T> {
 	 * @param value A value to register.
 	 */
 	public void register(String name, T value) {
+		int id = this.nextId++;
 		this.map.put(name, value);
+		this.byId.size(id + 1);
+		this.byId.set(id, value);
+		this.toId.put(value, id);
 	}
 
 	/**
@@ -59,7 +76,7 @@ public final class BasicRegistry<T> implements Codec<T> {
 	 * @return This registry's {@link #lifecycle}.
 	 */
 	@Nonnull
-	public Lifecycle getLifecycle() {
+	public Lifecycle registryLifecycle() {
 		return this.lifecycle;
 	}
 
@@ -70,7 +87,7 @@ public final class BasicRegistry<T> implements Codec<T> {
 	 * @return A value for a given {@link String} name, or null if there's no value for the given {@link String}.
 	 */
 	@Nullable
-	public T getValue(String name) {
+	public T get(String name) {
 		return this.map.get(name);
 	}
 
@@ -101,7 +118,7 @@ public final class BasicRegistry<T> implements Codec<T> {
 	 * @return A set of all the registered values in this registry.
 	 */
 	@Nonnull
-	public Set<T> getValues() {
+	public Set<T> values() {
 		return this.map.values();
 	}
 
@@ -111,7 +128,7 @@ public final class BasicRegistry<T> implements Codec<T> {
 	 * @return A set of all the entries in this registry.
 	 */
 	@Nonnull
-	public Set<Map.Entry<String, T>> getEntries() {
+	public Set<Map.Entry<String, T>> entrySet() {
 		return this.map.entrySet();
 	}
 
@@ -125,11 +142,48 @@ public final class BasicRegistry<T> implements Codec<T> {
 		return this.map.containsKey(name);
 	}
 
+	/**
+	 * Gets the id for a given value.
+	 *
+	 * @return The id for a given value, or -1 if the value isn't registered.
+	 */
+	@Override
+	public int getId(T value) {
+		return this.toId.getInt(value);
+	}
+
+	/**
+	 * Gets a registry value with the given id.
+	 *
+	 * @param id An id to check for a registry value with.
+	 * @return A registry value with the given id, or null if the id is out of range or no value is registered under the id.
+	 */
+	@Override
+	@Nullable
+	public T byId(int id) {
+		return id >= 0 && id < this.byId.size() ? this.byId.get(id) : null;
+	}
+
+	/**
+	 * Gets the number of values added to this registry.
+	 *
+	 * @return The number of values added to this registry.
+	 */
+	@Override
+	public int size() {
+		return this.map.size();
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return this.map.values().iterator();
+	}
+
 	@Override
 	public <U> DataResult<Pair<T, U>> decode(DynamicOps<U> ops, U input) {
 		return Codec.STRING.decode(ops, input).flatMap((encodedRegistryPair) -> {
 			String name = encodedRegistryPair.getFirst();
-			T value = this.getValue(name);
+			T value = this.get(name);
 			return value == null ? DataResult.error(() -> "Unknown registry key: " + name) : DataResult.success(Pair.of(value, encodedRegistryPair.getSecond()), this.lifecycle);
 		});
 	}
