0839d4f08d730792bdcf9348f4a4578a4d1b5a1a
SmellyModder <41268323+SmellyModder@users.noreply.github.com>
2025-03-03 22:47:06 -0500

    Update BlueprintChiseledBookShelfBlock

--- src/main/java/com/teamabnormals/blueprint/common/block/BlueprintChiseledBookShelfBlock.java
+++ src/main/java/com/teamabnormals/blueprint/common/block/BlueprintChiseledBookShelfBlock.java
@@ -18,45 +18,4 @@ public class BlueprintChiseledBookShelfBlock extends ChiseledBookShelfBlock {
 		return new BlueprintChiseledBookShelfBlockEntity(pos, state);
 	}
 
-	// TODO: Determine whether needed still
-	/*@Override
-	protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
-		BlockEntity blockEntity = level.getBlockEntity(pos);
-		if (blockEntity instanceof ChiseledBookShelfBlockEntity bookShelf) {
-			Optional<Vec2> optional = getRelativeHitCoordinatesForBlockFace(result, state.getValue(HorizontalDirectionalBlock.FACING));
-			if (optional.isEmpty()) {
-				return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
-			} else {
-				int i = this.getHitSlot(optional.get());
-				if (state.getValue(SLOT_OCCUPIED_PROPERTIES.get(i))) {
-					removeBook(level, pos, player, bookShelf, i);
-					return ItemInteractionResult.sidedSuccess(level.isClientSide);
-				} else {
-					ItemStack stack = player.getItemInHand(hand);
-					if (stack.is(ItemTags.BOOKSHELF_BOOKS)) {
-						addBook(level, pos, player, bookShelf, stack, i);
-						return ItemInteractionResult.sidedSuccess(level.isClientSide);
-					} else {
-						return ItemInteractionResult.CONSUME;
-					}
-				}
-			}
-		} else {
-			return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
-		}
-	}
-
-	public int getHitSlot(Vec2 vec2) {
-		int i = vec2.y >= 0.5F ? 0 : 1;
-		int j = getSection(vec2.x);
-		return j + i * 3;
-	}
-
-	private static int getSection(float x) {
-		if (x < 0.375F) {
-			return 0;
-		} else {
-			return x < 0.6875F ? 1 : 2;
-		}
-	}*/
 }
