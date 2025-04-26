faeb75e7b29debe21e2b4a46d4a2bada4ed87f9b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2025-04-26 09:22:25 -0500

    Misc datagen fixes

--- src/main/java/com/teamabnormals/blueprint/core/data/server/BlueprintRecipeProvider.java
+++ src/main/java/com/teamabnormals/blueprint/core/data/server/BlueprintRecipeProvider.java
@@ -2,11 +2,13 @@ package com.teamabnormals.blueprint.core.data.server;
 
 import com.teamabnormals.blueprint.core.other.tags.BlueprintItemTags;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.data.BlockFamily;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -161,6 +163,10 @@ public class BlueprintRecipeProvider extends RecipeProvider {
 	public static void trimRecipes(RecipeOutput output, ItemLike item, ItemLike copyItem) {
 		trimSmithing(output, item);
 		copySmithingTemplate(output, item, copyItem);
+	}
+
+	public static void generateRecipes(RecipeOutput output, BlockFamily family) {
+		generateRecipes(output, family, FeatureFlags.REGISTRY.allFlags());
 	}
 
 	public static ResourceLocation suffix(ResourceLocation rl, String suffix) {
